8f5f38a664609f75200a410ed70c557c688787e3
dev <blevaka@gmail.com>
2025-07-31 12:00:39 +0700

    shift buttons from left

--- WinForms.fs
+++ WinForms.fs
@@ -7,14 +7,16 @@ type DialogButtons(dialog: Form) as x =
         inherit FlowLayoutPanel()
         let accept = new Button(DialogResult = DialogResult.OK, Text = "OK")
         let cancel = new Button(DialogResult = DialogResult.Cancel, Text = "Cancel")
-        
+        let empty = new Label(Width = 45)
         do
             x.AutoSize <- true
             x.AutoSizeMode <- AutoSizeMode.GrowAndShrink
             x.WrapContents <- false
+            x.Controls.Add empty
+
             x.Controls.Add accept
             x.Controls.Add cancel
-          
+
             dialog.AcceptButton <- accept
             dialog.CancelButton <- cancel
     end
